b9991d41a4e205dc12dd68237f984b5b6c560440
AustinBenSimiyu <rubberducktoo@gmail.com>
2023-02-19 07:17:25 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'The program's description'
+*
+* Return: Always return 0 (success)
+*/
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
+
